1bcf2b8ebd11fb189a0370b98370f6f35b0c06b9
taeleenz38 <taekyu.lee3@gmail.com>
2024-04-10 14:35:52 +1000

    color palette fixed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,15 +7,15 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    colors: {
-      primary: '#8A69DF',
-      black: '#000000',
-    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        primary: "#8A69DF",
+        black: "#000000",
       },
     },
   },
